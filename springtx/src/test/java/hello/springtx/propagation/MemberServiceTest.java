d956981b516294ab677aed77cdc89609fd207537
jaeshim <jaeshim@gmarket.com>
2023-07-23 22:33:52 +0900

    20230723

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 @Slf4j
 @SpringBootTest
@@ -79,7 +80,6 @@ class MemberServiceTest {
    */
   @Test
   void outerTxOn_success() {
-    //given
     String username="outerTxOn_success";
 
     //when
@@ -91,7 +91,7 @@ class MemberServiceTest {
   }
 
   /**
-   * memberService    @Transactional: OFF
+   * memberService    @Transactional: ON
    * memberRepository @Transactional: ON
    * logRepository    @Transactional: ON Exception
    */
@@ -106,6 +106,43 @@ class MemberServiceTest {
 
     //then : 모든 데이터가 롤백된다.
     Assertions.assertTrue(memberRepository.find(username).isEmpty());
+    Assertions.assertTrue(logRepository.find(username).isEmpty());
+  }
+
+  /**
+   * memberService    @Transactional: ON
+   * memberRepository @Transactional: ON
+   * logRepository    @Transactional: ON Exception
+   */
+  @Test
+  void recoverException_fail() {
+    //given
+    String username="로그예외_recoverException_fail";
+
+    //when
+    assertThatThrownBy(() -> memberService.joinV2(username))
+        .isInstanceOf(UnexpectedRollbackException.class);
+
+    //then : 모든 데이터가 롤백된다.
+    Assertions.assertTrue(memberRepository.find(username).isEmpty());
+    Assertions.assertTrue(logRepository.find(username).isEmpty());
+  }
+
+  /**
+   * memberService    @Transactional: ON
+   * memberRepository @Transactional: ON
+   * logRepository    @Transactional: ON(REQUIRES_NEW) Exception
+   */
+  @Test
+  void recoverException_success() {
+    //given
+    String username="로그예외_recoverException_success";
+
+    //when
+    memberService.joinV2(username);
+
+    //then : member 저장, log 롤백
+    Assertions.assertTrue(memberRepository.find(username).isPresent());
     Assertions.assertTrue(logRepository.find(username).isEmpty());
   }
 
